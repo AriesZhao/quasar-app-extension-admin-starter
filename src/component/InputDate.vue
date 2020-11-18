5dcb1d1c81870b4ee234f88d7a6dba95b4ca3b9a
AriesPSBC <aries.zhao@outlook.com>
2020-11-18 17:57:33 +0800

    commit

--- src/component/InputDate.vue
+++ src/component/InputDate.vue
@@ -11,17 +11,17 @@
     <template v-slot:prepend>
       <q-icon name="event" class="cursor-pointer">
         <q-popup-proxy ref="qDateProxy" v-if="!readonly">
-          <q-date v-model="dateValue" @input="onInput" />
+          <q-date v-model="dateValue" @input="onInput" :mask="patternValue" />
         </q-popup-proxy>
       </q-icon>
     </template>
 
-    <template v-slot:append v-if="showTime">
-      <q-icon name="access_time" class="cursor-pointer">
+    <template v-slot:append v-if="showTime || showHour">
+      <q-icon name="access_time" class="cursor-pointer" v-if="showTime">
         <q-popup-proxy ref="qTimeProxy" v-if="!readonly">
           <q-time
             v-model="dateValue"
-            mask="YYYY-MM-DD HH:mm"
+            :mask="patternValue"
             :hour-options="hourOptions"
             :minute-options="minuteOptions"
             format24h
@@ -32,6 +32,14 @@
           </q-time>
         </q-popup-proxy>
       </q-icon>
+      <q-select
+        v-model.number="hourValue"
+        v-if="showHour"
+        :options="hourArray"
+        emit-value
+        map-options
+        @input="hourChange"
+      />
     </template>
   </q-input>
 </template>
@@ -68,11 +76,23 @@ const hours = [
   24,
 ];
 
+const DEFAULT_PATTERN = "YYYY-MM-DD";
+
 export default {
   name: "InputDate",
   mixins: [input],
   props: {
+    //日期显示格式
+    pattern: {
+      type: String,
+      default: DEFAULT_PATTERN,
+    },
+    //是否支持时间
     showTime: {
+      type: Boolean,
+      default: false,
+    },
+    showHour: {
       type: Boolean,
       default: false,
     },
@@ -92,6 +112,9 @@ export default {
   data() {
     return {
       dateValue: null,
+      hourValue: 12,
+      hourArray: [],
+      patternValue: null,
     };
   },
   watch: {
@@ -101,18 +124,48 @@ export default {
   },
   mounted() {
     this.dateValue = this.value;
-    if (!this.dateValue && !this.showTime) {
+    this.patternValue = this.pattern;
+    if (this.showTime && this.patternValue === DEFAULT_PATTERN) {
+      this.patternValue = DEFAULT_PATTERN + " HH:mm";
+    }
+    //日期没有指定，则指定为当前日期
+    if (!this.dateValue) {
       let timeStamp = Date.now();
-      this.dateValue = date.formatDate(timeStamp, "YYYY-MM-DD HH:mm");
-    } else if (!this.dateValue && this.showTime) {
-      let timeStamp = Date.now();
-      this.dateValue = date.formatDate(timeStamp, "YYYY-MM-DD HH:mm");
+      this.dateValue = date.formatDate(timeStamp, this.patternValue);
+    }
+    //设置小时选项
+    if (this.showHour) {
+      this.hourArray = [];
+      this.hourOptions.forEach((item) => {
+        if (item < 6) {
+          this.hourArray.push({ label: `凌晨${item}时`, value: item });
+        } else if (item >= 6 && item <= 12) {
+          this.hourArray.push({ label: `上午${item}时`, value: item });
+        } else if (item > 12 && item <= 18) {
+          this.hourArray.push({ label: `下午${item}时`, value: item });
+        } else if (item > 18) {
+          this.hourArray.push({ label: `晚上${item}时`, value: item });
+        }
+      });
     }
   },
   methods: {
     onInput(e) {
       this.$refs.qDateProxy.hide();
       this.$emit("change", this.dateValue);
+    },
+    //小时变更
+    hourChange(hour) {
+      let timeStamp = date.extractDate(this.dateValue, this.patternValue);
+      timeStamp = date.adjustDate(timeStamp, {
+        hours: hour,
+        minutes: 0,
+        seconds: 0,
+      });
+      this.$emit(
+        "change",
+        date.formatDate(timeStamp, DEFAULT_PATTERN + " HH:mm:ss")
+      );
     },
   },
 };
